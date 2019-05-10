559a58a8e03e6145d6daa0f42cb3f67fcc77efa6
rafal-markowski <rafal_markowski@onet.pl>
2019-05-10 09:26:16 +0200

    Stworzenie sekcji Services

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './styles/main.scss';
 
 import './images/logo.png';
+import './images/service_1.png';
+import './images/service_2.png';
+import './images/service_3.png';
+import './images/service_4.png';
